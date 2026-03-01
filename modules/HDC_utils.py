1cd2e0ee8066a16df1e387dafb6767ad104f638d
James Fleming <jamesfleming.sd@gmail.com>
2026-02-28 18:45:56 -0800

    change to gaussian orthogonal RP matrix (test)

--- modules/HDC_utils.py
+++ modules/HDC_utils.py
@@ -315,7 +315,15 @@ class DensityModel(nn.Module):
                 torch.cuda.manual_seed(42)
                 torch.cuda.manual_seed_all(42)
 
-            self.projection = embeddings.Projection(self.input_dim, self.hd_dim)
+            # self.projection = embeddings.Projection(self.input_dim, self.hd_dim)
+
+            self.projection = nn.Linear(self.input_dim, self.hd_dim, bias=False)
+            with torch.no_grad():
+                shape = (self.hd_dim, self.input_dim)
+                gaussian_matrix = torch.randn(shape)
+                q, r = torch.linalg.qr(gaussian_matrix)
+                orthogonal_map = q * torch.sqrt(torch.tensor(self.hd_dim, dtype=torch.float32))
+                self.projection.weight.copy_(orthogonal_map)
 
             torch.set_rng_state(torch_rng_state) # set back to random
             np.random.set_state(numpy_rng_state)
