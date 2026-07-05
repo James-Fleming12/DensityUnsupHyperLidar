8148c36502affbac3956dc9a8e8c11ecfadd3bab
James Fleming <jamesfleming.sd@gmail.com>
2026-07-05 07:56:43 -0700

    limited the number of sequences

--- unsup_update_test.py
+++ unsup_update_test.py
@@ -49,7 +49,7 @@ def main():
         quit()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    train_seqs = DATA["split"]["train"][:4]
+    train_seqs = DATA["split"]["train"][:1]
     valid_seqs = DATA["split"]["valid"]
 
     print("Building SemanticKITTI clean baseline parser...")
